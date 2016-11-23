ffe16e7d7a071e722a7d1582a894bf0daf80eac4
Ian <152772a@mymail.nyp.edu.sg>
2016-11-23 15:16:05 +0800

    i

--- app/src/main/java/com/example/ian/mocktestpracticelistviewcontexdt/MainActivity.java
+++ app/src/main/java/com/example/ian/mocktestpracticelistviewcontexdt/MainActivity.java
@@ -2,18 +2,36 @@ package com.example.ian.mocktestpracticelistviewcontexdt;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import static android.widget.ArrayAdapter.createFromResource;
 
 public class MainActivity extends AppCompatActivity {
+
+    ListView li;
+    ArrayAdapter<CharSequence> arr;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        li = (ListView) findViewById(R.id.listt);
+
+        arr = createFromResource(this, R.array.lis, android.R.layout.simple_spinner_dropdown_item);
+        li.setAdapter(arr);
 
 
-
-
+        li.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Toast.makeText(getApplicationContext(), li.getItemAtPosition(i).toString(), Toast.LENGTH_SHORT).show();
+            }
+        });
 
     }
 }
